44bb680d8a7d9f4c778e7e366942fd93900174e0
Ahmad <153628471+ArcticNomad@users.noreply.github.com>
2025-01-23 18:52:11 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/nike-landing-page/',
+  base: '/Nike-Landing-Page/',
   plugins: [react()],
 })
